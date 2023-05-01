25bfdbdb53141bdd71e0a5fd1a3c0baf0a57d132
cjax21 <CJackson21@kgi.edu>
2023-05-01 13:04:12 -0700

    changed extract and create_labels

--- preprocessing/extract.py
+++ preprocessing/extract.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from pandas.core.frame import DataFrame
+
+
 
 def extract_data(filepath: str):
     '''
@@ -10,5 +13,24 @@ def extract_data(filepath: str):
         df (DataFrame): pandas dataframe containing the csv data
     '''
     df = pd.read_csv(filepath)
+    
+    return df
+
+def transform(df: DataFrame) -> DataFrame:
+    '''
+    cleans data
+
+    args:
+        df (DataFrame): pandas dataframe containing the raw data
+    
+    returns:
+        df (DataFrame): pandas dataframe containing the clean data
+    '''
+
+    # drop null values
+    df.dropna(inplace=True)
+
+    # remove decimal from year column and convert to string
+    df.Year = df.Year.astype('int').astype("str")
     
     return df
